1689278cee9b5d8814ac91c829372b883cf98476
Mauro Gestoso <maurogestoso@gmail.com>
2017-12-14 16:09:22 +0000

    Add authUser state

--- src/components/App.js
+++ src/components/App.js
@@ -13,38 +13,54 @@ import HomePage from './HomePage';
 import AccountPage from './AccountPage';
 
 import * as routes from '../constants/routes';
+import { firebase } from '../firebase';
 
-const App = () => (
-  <Router>
-    <div>
-      <Navigation />
-      <hr/>
-      <Route 
-        exact path={routes.LANDING}
-        component={() => <LandingPage />}
-      />
-      <Route 
-        exact path={routes.SIGN_UP}
-        component={() => <SignUpPage />}
-      />
-      <Route 
-        exact path={routes.SIGN_IN}
-        component={() => <SignInPage />}
-      />
-      <Route 
-        exact path={routes.PASSWORD_FORGET}
-        component={() => <PasswordForgetPage />}
-      />
-      <Route 
-        exact path={routes.HOME}
-        component={() => <HomePage />}
-      />
-      <Route 
-        exact path={routes.ACCOUNT}
-        component={() => <AccountPage />}
-      />
-    </div>
-  </Router>
-);
+class App extends React.Component {
+  state = { authUser: null }
+
+  componentDidMount () {
+    firebase.auth.onAuthStateChanged(authUser => {
+      authUser
+        ? this.setState(() => ({ authUser }))
+        : this.setState(() => ({ authUser: null }));
+    })
+  }
+
+  render () {
+    const { authUser } = this.state;
+    return (
+      <Router>
+        <div>
+          <Navigation authUser={authUser} />
+          <hr/>
+          <Route 
+            exact path={routes.LANDING}
+            component={() => <LandingPage />}
+          />
+          <Route 
+            exact path={routes.SIGN_UP}
+            component={() => <SignUpPage />}
+          />
+          <Route 
+            exact path={routes.SIGN_IN}
+            component={() => <SignInPage />}
+          />
+          <Route 
+            exact path={routes.PASSWORD_FORGET}
+            component={() => <PasswordForgetPage />}
+          />
+          <Route 
+            exact path={routes.HOME}
+            component={() => <HomePage />}
+          />
+          <Route 
+            exact path={routes.ACCOUNT}
+            component={() => <AccountPage />}
+          />
+        </div>
+      </Router>
+    );
+  }
+}
 
 export default App;
